8e8d18d9af1ba787df54e45003cfb3bb7f30d3fb
Шемонаев Илья Павлович <ovelrdose@mcr.ru>
2026-06-22 15:52:28 +0700

    fix: add media proxy to nginx

--- backend/portal_summer/urls.py
+++ backend/portal_summer/urls.py
@@ -26,3 +26,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # В production тоже нужно отдавать медиа!
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
